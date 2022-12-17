87b1479c994b1ac4e9e1b72a1fdb21cf41d0df78
Marshall Polaris <marshall@pol.rs>
2022-12-17 15:27:39 -0800

    Use Supabase to power "add user to group" dropdown

--- web/lib/supabase/users.ts
+++ web/lib/supabase/users.ts
@@ -1,12 +1,19 @@
 import { db } from './db'
 import { run } from 'common/supabase/utils'
+import { User } from 'common/user'
+
+export type SearchUserInfo = Pick<
+  User,
+  'id' | 'name' | 'username' | 'avatarUrl'
+>
 
 export async function searchUsers(prompt: string) {
   const { data } = await run(
     db
       .from('users')
-      .select('data, id')
+      .select('id, data->name, data->username, data->avatarUrl')
       .or(`data->>username.ilike.%${prompt}%,data->>name.ilike.%${prompt}%`)
+      .order('data->name')
   )
-  return data
+  return data as SearchUserInfo[]
 }
